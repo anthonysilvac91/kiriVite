d738a64040029eda1485e58942e935c43435df1f
Anthony Silva <anthony.silvac@gmail.com>
2024-08-25 21:15:34 -0400

    se agrega componente call to action

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import ProductList from '../../Components/Products/ProductList'
 import { useProductContext } from '../../Context/ProductContext';
 import Category from '../../Components/Category/Category';
 import img_banner from '../../../src/Assets/banner_stabilo.jpg'
+import CallToAction from '../../Components/CallToAction/CallToAction';
 
 const Home = () => {
   const { state } = useProductContext();
@@ -14,19 +15,16 @@ const Home = () => {
         <img className='w-full' src={img_banner} alt="banner_stabilo" />
       </div>
       <div className=' flex justify-center flex-col lg:ml-10 lg:mr-10'>
-
         <h1 className='text-base mt-2 font-bold ml-4 2xl:ml-10 md:text-2xl lg:text-3xl 2xl:text-4xl 2xl:mt-5 '>Categorías</h1>
-
         <Category/>
-
-
         <h1 className='text-lg font-bold ml-4 2xl:ml-10 md:text-2xl lg:text-3xl 2xl:text-4xl 2xl:mt-5 lg:mt-2 lg:mr-10'>Productos Destacados</h1>
-
         <div className='grid grid-cols-2 gap-4 m-4 2xl:ml-10 2xl:mr-10 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 2xl:gap-10'>
            <ProductList products={state.products.slice(0, 10)} />
         </div>
-
+        <div>
+          <CallToAction/>
         </div>
+      </div>
 
     
     </>
